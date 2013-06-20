cb50f0cb62eb3be5ca8f26f1450587a8b49b4da9
YuuShigetani <s2g4t1n2@gmail.com>
2013-06-20 14:27:48 +0900

    fix

--- sample/spec/fabs_spec.c
+++ sample/spec/fabs_spec.c
@@ -5,51 +5,39 @@
 /* Define tests */
 mpispec_def(fabs_spec)
 
-  if(MPIUT_RANK == 0) {
-    describe(fabs, "標準ライブラリfabsの")
+  describe(fabs, "標準ライブラリfabsの")
 
-      context("入力が正の数の時")
-        it("同じ数値を返す")
-          expect_to_equal( fabs(1.0), 1.0)
-          expect_to_equal( fabs(0.0), 0.0)
-          expect_to_equal( fabs(6.7), 6.7)
-        end_it
-      end
+    context("入力が正の数の時")
+      it("同じ数値を返す")
+        expect_to_equal( fabs(1.0), 1.0)
+        expect_to_equal( fabs(0.0), 0.0)
+        expect_to_equal( fabs(6.7), 6.7)
+      end_it
+    end
 
-      context("入力が負の数の時")
-        it("正の数を返す")
-          expect_to_equal( fabs(-1.1), 1.0)
-          expect_to_equal( fabs(-6.7), 6.7)
-        end_it
-      end
+    context("入力が負の数の時")
+      it("正の数を返す")
+        expect_to_equal( fabs(-1.1), 1.0)
+        expect_to_equal( fabs(-6.7), 6.7)
+      end_it
+    end
 
-    end_describe
-    describe(fab, "標準ライブラリfabsの")
+  end_describe
 
-      context("入力が正の数の時")
-        it("同じ数値を返す")
-          expect_to_equal( fabs(1.0), 1.0)
-          expect_to_equal( fabs(0.0), 0.0)
-          expect_to_equal( fabs(6.7), 6.7)
-        end_it
-      end
+end_def
 
-    end_describe
-    /* CSpec_Run( description( fabs ), CSpec_NewOutputUnit() ); */
-  }
-  else if(MPIUT_RANK == 1) {
-    describe(fabs, "標準ライブラリfabsの")
+mpispec_def(fab_spec)
 
-      context("入力が負の数の時")
-        it("正の数を返す")
-          expect_to_equal( fabs(-1.1), 1.0)
-          expect_to_equal( fabs(-6.7), 6.7)
-        end_it
-      end
+  describe(fab, "標準ライブラリfabsの")
 
-    end_describe
-  }
-  else {
-  }
+    context("入力が正の数の時")
+      it("同じ数値を返す")
+        expect_to_equal( fabs(1.0), 1.0)
+        expect_to_equal( fabs(0.0), 0.0)
+        expect_to_equal( fabs(6.7), 6.7)
+      end_it
+    end
+
+  end_describe
 
 end_def
--- sample/spec/main_spec.c
+++ sample/spec/main_spec.c
@@ -3,33 +3,18 @@
 
 #include "mpispec/mpispec.h"
 
-/* Prototypes */
-/* mpispec_prototype(fabs_spec) */
-/* #<{(| MPIUT_PROTOTYPE(test_cpi); |)}># */
-/* #<{(| MPIUT_PROTOTYPE(test_integral); |)}># */
-/* #<{(| MPIUT_PROTOTYPE(test_mpiassertion); |)}># */
-/*  */
-/* #<{(| Register |)}># */
-/* mpispec_init */
-/*  */
-/* mpispec_register("mpi fabs", fabs_spec) */
-/* #<{(| MPIUT_REGISTER("mpi cpi", test_cpi); |)}># */
-/* #<{(| MPIUT_REGISTER("mpi integral", test_integral); |)}># */
-/* #<{(| MPIUT_REGISTER("mpi assertion", test_mpiassertion); |)}># */
-/*  */
-/* mpispec_finalize */
-
 mpispec_prototype(fabs_spec)
-int main(int argc, char **argv)
-{
-  MPI_Init(&argc, &argv);
+mpispec_prototype(fab_spec)
 
-  mpispec_setup();
+mpispec_init
 
-  fabs_spec();
-
-  MPI_Barrier(MPI_COMM_WORLD);
-  mpispec_show_result();
-  MPI_Finalize();
-  return EXIT_SUCCESS;
+if(MPIUT_RANK == 0) {
+  MPISpec_Run(fabs_spec);
 }
+else if(MPIUT_RANK == 1) {
+  MPISpec_Run(fab_spec);
+}
+else {
+}
+
+mpispec_finalize
