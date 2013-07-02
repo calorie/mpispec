1883291a23c63ed5fb3bb963b43265f050c1293d
YuuShigetani <s2g4t1n2@gmail.com>
2013-07-02 20:59:41 +0900

    rm dir

--- test/mpiunit/run-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-make
-sudo make install
-cd sample/
-mpiut -np 2
-cd ..
--- test/procunit/run-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-gmake
-gmake install
-cd sample/
-proclog -np 2
-procut rank0.log
-rm -f *.log
-cd ..
