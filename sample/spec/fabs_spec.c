0a383e8c07038182a1e100f37bb06d718d28a2d9
YuuShigetani <s2g4t1n2@gmail.com>
2013-08-06 23:49:38 +0900

    fix fabs spec

--- sample/spec/fabs_spec.c
+++ sample/spec/fabs_spec.c
@@ -4,36 +4,36 @@
 
 int a = 0;
 
-mpispec_def(fabs_spec)
+mpispec_def( fabs_spec )
 
-  describe("標準ライブラリfabsの")
+  describe( "標準ライブラリfabsの" )
 
-    context("入力が0の時")
+    context( "入力が0の時" )
 
-      it("同じ数値を返す")
-        expect_to_equal( fabs(a), a)
+      it( "同じ数値を返す" )
+        expect_to_equal( fabs(a), a )
       end
 
     end_context
 
-    before(fabs)
+    before( fabs )
 
       a = rand() % 11;
 
     end_before
 
-    context("入力が正の数の時")
+    context( "入力が正の数の時" )
 
-      it("同じ数値を返す")
-        expect_to_equal( fabs(a), a)
+      it( "同じ数値を返す" )
+        expect_to_equal( fabs(a), a )
       end
 
     end_context
 
-    context("入力が負の数の時")
+    context( "入力が負の数の時" )
 
-      it("正の数を返す")
-        expect_to_equal( fabs(-a), a)
+      it( "正の数を返す" )
+        expect_to_equal( fabs(-a), a )
       end
 
     end_context
